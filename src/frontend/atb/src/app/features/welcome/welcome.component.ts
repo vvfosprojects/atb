5bea5e0910f10a10f7978a2c3fb654d42204a705
Davide <cappadavide@gmail.com>
2020-03-20 12:59:19 +0100

    Fix: aggiunto delay al dispatcher di welcome

--- src/frontend/atb/src/app/features/welcome/welcome.component.ts
+++ src/frontend/atb/src/app/features/welcome/welcome.component.ts
@@ -5,6 +5,7 @@ import { AuthState } from '../auth/store/auth.state';
 import { UserInterface } from '../../shared/interface/common/user.interface';
 import { Roles } from '../../shared/enum/roles.enum';
 import { Navigate } from '@ngxs/router-plugin';
+import { delay } from 'rxjs/operators';
 
 @Component({ template: `` })
 export class WelcomeComponent implements OnDestroy {
@@ -14,7 +15,7 @@ export class WelcomeComponent implements OnDestroy {
     private subscription = new Subscription();
 
     constructor(private store: Store) {
-        this.subscription.add(this.currentUser$.subscribe((r: UserInterface) => this.dispatchUser(r)));
+        this.subscription.add(this.currentUser$.pipe(delay(100)).subscribe((r: UserInterface) => this.dispatchUser(r)));
     }
 
     ngOnDestroy(): void {
@@ -22,14 +23,18 @@ export class WelcomeComponent implements OnDestroy {
     }
 
     dispatchUser(user: UserInterface) {
-        if (!user) {
-            return
+        if (user) {
+            console.log('dispatchUser');
+            // if (!user) {
+            //     return
+            // }
+            if (user.roles.length === 0) {
+                console.log('non ci sono ruoli');
+                // Todo user senza ruoli
+            }
+            (user.roles.includes(Roles.Manager) && user.roles.includes(Roles.Doctor)) && this.store.dispatch(new Navigate([ '/reports' ]));
+            user.roles.includes(Roles.Manager) && this.store.dispatch(new Navigate([ '/reports' ]));
+            user.roles.includes(Roles.Doctor) && this.store.dispatch(new Navigate([ '/home' ]));
         }
-        if (user.roles.length === 0) {
-            // Todo user senza ruoli
-        }
-        (user.roles.includes(Roles.Manager) && user.roles.includes(Roles.Doctor)) && this.store.dispatch(new Navigate(['/reports']));
-        user.roles.includes(Roles.Manager) && this.store.dispatch(new Navigate(['/reports']));
-        user.roles.includes(Roles.Doctor) && this.store.dispatch(new Navigate(['/home']));
     }
 }
