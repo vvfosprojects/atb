7cfd3064d90f2ca87879399cbbf89daec442ebbc
notmikenot <micheledragonetti99@gmail.com>
2020-03-23 14:16:03 +0100

    Il metodo "formatDate" ora aggiunge "T12:00:00.000Z" alla data

--- src/frontend/atb/src/app/shared/functions/functions.ts
+++ src/frontend/atb/src/app/shared/functions/functions.ts
@@ -2,7 +2,7 @@ export function formatDate(data: any) {
     const year = data.year;
     const month = data.month.toString().length > 1 ? data.month : '0' + data.month;
     const day = data.day.toString().length > 1 ? data.day : '0' + data.day;
-    return year + '-' + month + '-' + day;
+    return year + '-' + month + '-' + day + 'T12:00:00Z';
 }
 
 export function formatDateForNgbDatePicker(data: any) {
