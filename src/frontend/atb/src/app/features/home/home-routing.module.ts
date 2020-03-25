ce910968815be6a4bd67804831ef734e3cbe9d2f
Davide Cappa <cappadavide@gmail.com>
2020-03-25 10:20:49 +0100

    Add: aggiunto componente dummy "data-tables"

--- src/frontend/atb/src/app/features/home/home-routing.module.ts
+++ src/frontend/atb/src/app/features/home/home-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { FormPositivoComponent } from './form-positivo/form-positivo.component';
 import { FormAssenteComponent } from './form-assente/form-assente.component';
+import { DataTablesComponent } from './data-tables/data-tables.component';
 
 const routes: Routes = [
     {
@@ -29,6 +30,10 @@ const routes: Routes = [
     {
         path: 'form-assente/:id',
         component: FormAssenteComponent
+    },
+    {
+        path: 'data-tables',
+        component: DataTablesComponent
     }
 ];
 
