6d4af96797b9dbaa703891c5ab8cf2a6bf49f743
Marcello Esposito <esposito.marce@gmail.com>
2020-03-25 14:47:36 +0100

    Change password command auditing obscured

--- src/backend/DomainModel/CQRS/Commands/ChangePassword/ChangePasswordCommand.cs
+++ src/backend/DomainModel/CQRS/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -1,9 +1,21 @@
-﻿namespace DomainModel.CQRS.Commands.ChangePassword
+﻿using CQRS.Logging;
+
+namespace DomainModel.CQRS.Commands.ChangePassword
 {
-    public class ChangePasswordCommand
+    public class ChangePasswordCommand : IHasCustomAudit
     {
         public string Username { get; set; }
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
+
+        public string SerializeForAudit()
+        {
+            return this.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{{ Username: \"{ this.Username }\", OldPwd: \"***\", NewPwd: \"***\" }}";
+        }
     }
 }
