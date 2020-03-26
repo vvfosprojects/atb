5dc062a5f2b3c48bbde7b53f51f67c33abb2c0a1
Davide <cappadavide@gmail.com>
2020-03-26 21:42:11 +0100

    Fix: semplificata globalSorter

--- src/frontend/atb/src/app/shared/functions/sorter-case.ts
+++ src/frontend/atb/src/app/shared/functions/sorter-case.ts
@@ -2,19 +2,13 @@ import { SuspectCaseInterface } from '../interface/suspect-case.interface';
 import { PositiveCaseInterface } from '../interface/positive-case.interface';
 
 export function globalSorter(a: SuspectCaseInterface | PositiveCaseInterface, b: SuspectCaseInterface | PositiveCaseInterface) {
-    let d = { HOME: 2, HOSP: 1, INTCARE: 0 };
+    const d = { HOME: 2, HOSP: 1, INTCARE: 0 };
 
-    if (a.data.quarantinePlace != b.data.quarantinePlace) {
-        if (d[a.data.quarantinePlace] > d[b.data.quarantinePlace])
-            return 1;
-        else if (d[a.data.quarantinePlace] < d[b.data.quarantinePlace])
-            return -1;
-        return 0;
-    } else {
-        if (a.subject.number < b.subject.number)
-            return 1;
-        else if (a.subject.number > b.subject.number)
-            return -1;
-        else return 0;
-    }
+    if (d[a.data.quarantinePlace] > d[b.data.quarantinePlace]) return 1;
+    if (d[a.data.quarantinePlace] < d[b.data.quarantinePlace]) return -1;
+
+    if (a.subject.number < b.subject.number) return 1;
+    if (a.subject.number > b.subject.number) return -1;
+
+    return 0;
 }
