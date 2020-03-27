cf3058706ab3c5c646f5dccd3ac88c5929456a96
Davide Cappa <cappadavide@gmail.com>
2020-03-27 11:27:35 +0100

    Add: aggiunta configuration productionTest in angular.json, modificati i script di build e aggiunta evidenza della versione di test nel footer.

--- src/frontend/atb/src/app/features/footer/footer.component.ts
+++ src/frontend/atb/src/app/features/footer/footer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: [ './footer.component.scss' ]
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+    version: string;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.version = environment.productionTest ? ' (test)' : '';
+    }
 
 }
