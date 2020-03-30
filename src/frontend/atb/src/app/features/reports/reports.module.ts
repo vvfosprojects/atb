0ffd72f5ff4e2bf42a48e2924ec0dea017a86158
Davide <cappadavide@gmail.com>
2020-03-30 12:17:57 +0200

    Add: inizio implementazione statistics, creato store e service, importati nel relativo modulo

--- src/frontend/atb/src/app/features/reports/reports.module.ts
+++ src/frontend/atb/src/app/features/reports/reports.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SharedModule } from '../../shared/shared.module';
+import { StatisticsService } from '../../core/services/statistics/statistics.service';
+import { NgxsModule } from '@ngxs/store';
+import { StatisticsState } from './store/statistics.state';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
     declarations: [ StatisticsComponent ],
     imports: [
         CommonModule,
         ReportsRoutingModule,
-        SharedModule
-    ]
+        SharedModule,
+        NgxsModule.forFeature([ StatisticsState ]),
+        NgxChartsModule
+    ],
+    providers: [ StatisticsService ]
 })
 export class ReportsModule {
 }
---
+++ src/frontend/atb/src/app/features/reports/store/statistics.actions.ts
@@ -0,0 +1,7 @@
+export class GetStatisticsData {
+    static readonly type = '[Statistics] Get Statistics Data';
+}
+
+export class ClearStatisticsData {
+    static readonly type = '[Statistics] Clear Statistics Data';
+}
