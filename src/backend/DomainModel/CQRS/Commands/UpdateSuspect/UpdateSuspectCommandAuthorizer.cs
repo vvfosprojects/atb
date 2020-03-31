59feabdd6d2e07747f5b1779154659b383c67dda
matteo nocco <N.M.90@hotmail.it>
2020-03-31 16:22:49 +0200

    Aggiunto validator e authorizer per UpdateSuspect

---
+++ src/backend/DomainModel/CQRS/Commands/UpdateSuspect/UpdateSuspectCommandAuthorizer.cs
@@ -0,0 +1,25 @@
+﻿using CQRS.Authorization;
+using CQRS.Commands.Authorizers;
+using DomainModel.Services.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.CQRS.Commands.UpdateSuspect
+{
+    public class UpdateSuspectCommandAuthorizer : ICommandAuthorizer<UpdateSuspectCommand>
+    {
+        private readonly IGetSessionContext getSessionContext;
+
+        public UpdateSuspectCommandAuthorizer(IGetSessionContext getSessionContext)
+        {
+            this.getSessionContext = getSessionContext;
+        }
+
+        public IEnumerable<AuthorizationResult> Authorize(UpdateSuspectCommand command)
+        {
+            if (!getSessionContext.LoggedUserIsDoctor())
+                yield return new AuthorizationResult("Unauthorized");
+        }
+    }
+}
