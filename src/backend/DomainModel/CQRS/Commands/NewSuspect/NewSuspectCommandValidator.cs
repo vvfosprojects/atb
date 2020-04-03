2b4a11a08d913fa449e9a9e82ef8944514087d27
Marcello Esposito <esposito.marce@gmail.com>
2020-04-03 10:27:02 +0200

    Fix
    Sheet number can be null.
    Name can be null.
    Surname can be null.

--- src/backend/DomainModel/CQRS/Commands/NewSuspect/NewSuspectCommandValidator.cs
+++ src/backend/DomainModel/CQRS/Commands/NewSuspect/NewSuspectCommandValidator.cs
@@ -12,7 +12,7 @@ namespace DomainModel.CQRS.Commands.NewSuspectCommand
             const string regexEmail = @"\A(?:[a-zA-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)\Z";
             const string regex = "^[a-zA-Z ]*$";
 
-            if (command.Number < 0)
+            if (command.Number.HasValue && command.Number <= 0)
             {
                 yield return new ValidationResult("The case number must be positive");
                 yield break;
@@ -24,13 +24,13 @@ namespace DomainModel.CQRS.Commands.NewSuspectCommand
                 yield break;
             }
 
-            if (!Regex.IsMatch(command.Name, regex))
+            if (!string.IsNullOrWhiteSpace(command.Name) && !Regex.IsMatch(command.Name, regex))
             {
                 yield return new ValidationResult("The Name is not in a valid format");
                 yield break;
             }
 
-            if (!Regex.IsMatch(command.Surname, regex))
+            if (!string.IsNullOrWhiteSpace(command.Surname) && !Regex.IsMatch(command.Surname, regex))
             {
                 yield return new ValidationResult("The Surname is not in a valid format");
                 yield break;
